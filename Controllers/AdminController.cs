5515bcb60deeca7d8343c5b6e9e0822cf8668d97
shivba28 <shivba28@gmail.com>
2021-04-21 19:19:16 +0530

    changes made on 21/04/21 19:19

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View("AdminLogin");
+        }
+
+        [HttpPost]
+
+        public ActionResult AdminHome()
+        {
+            return View("AdminHome");
+        }
+    }
+
+}
