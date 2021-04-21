5a997a91d8a02e5e7e0d77cd2e8966bc744b2141
shivba28 <shivba28@gmail.com>
2021-04-21 09:11:56 +0530

    Game_Rental_MVCs

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginMVC.Models;
+
+namespace LoginMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(LoginMVC.Models.Customer userModel)
+        {
+            using (GameRentalEntities db = new GameRentalEntities())
+            {
+                var UserDetails = db.Customers.Where(x => x.email == userModel.email && x.password == userModel.password).FirstOrDefault();
+                if(UserDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong email or password";
+                    return View("Index", userModel);
+                }
+
+                else
+                {
+                    Session["CustomerID"] = UserDetails.customer_id;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginMVC.Models;
+
+namespace LoginMVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View("RegistrationView");
+        }
+
+        [HttpPost]
+        public ActionResult AuthorizeReg(LoginMVC.Models.Customer userModel)
+        {
+            using (GameRentalEntities db = new GameRentalEntities())
+            {
+                
+               /*if ( == userModel.email)
+                {
+                    userModel.LoginErrorMessage = "email already exist";
+                    return View("RegistrationView", userModel);
+                }*/
+                
+                    Customer cust = new Customer();
+                    cust.customer_name = userModel.customer_name;
+                    cust.dob = userModel.dob;
+                cust.gender = userModel.gender;
+                cust.contact = userModel.contact;
+                cust.email = userModel.email;
+                cust.password = userModel.password;
+
+                    db.Customers.Add(cust);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Login");
+             
+            }
+        }
+    }
+}
