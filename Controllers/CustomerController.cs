b05fd64fcf8d5a075778311241ade62957dd6fac
shivba28 <shivba28@gmail.com>
2021-04-28 01:37:49 +0530

    28/04/21 01:37

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -13,7 +13,7 @@ namespace LoginMVC.Controllers
     {
 
         // GET: Customer
-        public ActionResult Index(LoginMVC.Models.Game userModel)
+        public ActionResult Index()
         {
             using (OnlineGameRentalStoreEntities db = new OnlineGameRentalStoreEntities())
             {
@@ -24,11 +24,6 @@ namespace LoginMVC.Controllers
                 
         }
 
-        public ActionResult CustomerLogin()
-        {
-
-            return RedirectToAction("Index", "Login");
-        }
 
 
         [Authorize]
